298652df56e76e9adb82557869a965c94c5592f5
Tagir Asadullin <Tagirasadus@gmail.com>
2021-02-21 15:15:02 +0500

    changing button behaviour system

--- main.py
+++ main.py
@@ -14,9 +14,21 @@ screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
 size = width, height = screensize
 
 screen = pygame.display.set_mode(screensize)
-
 # current displaying window
-display_menu, display_levels, display_credits, is_playing, on_pause = True, False, False, False, False
+current_window = 0
+
+
+def inc_number(v):
+    global current_window
+    current_window += v
+
+
+buttons_behaviour = {'Credits': lambda x: inc_number(1), 'Return': lambda x: inc_number(-2),
+                     'Return to menu': lambda x: inc_number(-1),
+                     'Start game': lambda x: inc_number(2),
+                     'Return to main menu': lambda x: inc_number(-4), 'Exit': lambda x: exit(),
+                     'Tagir Asadullin': lambda x: webbrowser.open_new('t.me/ficusthepottedplant'),
+                     'Continue': lambda x: inc_number(-1)}
 
 LEVELS = 10  # levels count
 
@@ -72,30 +84,11 @@ class Button:
         if self.text_x <= mouse_pos[0] <= self.text_x + self.text_w and \
                 self.text_y <= mouse_pos[1] <= self.text_y + self.text_h and self.is_clickable and not self.is_locked:
             if pygame_event.type == pygame.MOUSEBUTTONDOWN:
-                # the values are display_menu, display_levels, display_credits, is_playing, on_pause
-                windows_behaviour = {'Credits': (False, False, True, False, False),
-                                     'Return': (True, False, False, False, False),
-                                     'Start game': (False, True, False, False, False),
-                                     'Return to main menu': (True, False, False, False, False)}
-
-                global display_menu, display_credits, display_levels, running, is_playing, on_pause
-                if not windows_behaviour.get(self.button_text) and display_levels:
-                    mouse.display = False
-                    display_menu, display_credits, display_levels = False, False, False
-                    is_playing = True
-
-                elif self.button_text == 'Exit':  # in pause and main menu
-                    running = False
-                    mouse.display = True
-                elif self.button_text == 'Tagir Asadullin':  # in credits
-                    webbrowser.open_new('t.me/ficusthepottedplant')
-                elif self.button_text == 'Continue':  # in pause
-                    is_playing, on_pause = True, False
-                    mouse.display = False
+                global current_window, buttons_behaviour
+                if self.button_text.isnumeric():
+                    current_window = 3
                 else:
-                    display_menu, display_levels, \
-                    display_credits, is_playing, on_pause = windows_behaviour[self.button_text]
-                    mouse.display = True
+                    buttons_behaviour[self.button_text](None)
 
     def unlock_button(self):
         """if the button locked, than unlock button by request """
@@ -109,9 +102,9 @@ class Menu:
 
     def __init__(self):
         self.font = pygame.font.Font('data/fonts/8-BIT WONDER.TTF', 6)
-        self.buttons = [Button('Start game', width // 2 - 200, height // 2 - 20, 40),
-                        Button('Credits', width // 2 - 100, height // 2 + 15 + 30, 30),
-                        Button('Exit', width // 2 - 35, height // 2 + 10 + 30 * 3, 20)]
+        self.buttons = [Button('Start game', width // 2 - 200 + 10, height // 2 - 20, 40),
+                        Button('Credits', width // 2 - 98 + 10, height // 2 + 15 + 30, 30),
+                        Button('Exit', width // 2 - 35 + 10, height // 2 + 10 + 30 * 3, 20)]
 
         self.matrix = []
         # generating Matrix effect
@@ -139,7 +132,6 @@ class Menu:
             i[1][1] += 3
             if i[1][1] >= height + 3:
                 self.matrix.remove(i)
-
         for i in range(20):
             self.falling_down()
         for i in self.buttons:
@@ -158,9 +150,9 @@ class Credits(Menu):
     def __init__(self):
         super().__init__()
         self.buttons = [Button('Made by', width // 2 - 140, height // 2 - 20, 40, False),
-                        Button('Alim Mullayanov', width // 2 - 225, height // 2 + 10 + 30, 30, False),
-                        Button('Tagir Asadullin', width // 2 - 202, height // 2 + 15 + 30 * 3, 30),
-                        Button('Return', width // 2 - 60, height // 2 + 10 + 30 * 9, 20)]
+                        Button('Alim Mullayanov', width // 2 - 225+ 10, height // 2 + 10 + 30, 30, False),
+                        Button('Tagir Asadullin', width // 2 - 202+ 10, height // 2 + 15 + 30 * 3, 30),
+                        Button('Return to menu', width // 2 - 135+ 10, height // 2 + 10 + 30 * 9, 20)]
 
     def render(self):
         """what to do on game cycle"""
@@ -173,8 +165,8 @@ class LevelChooser(Menu):
 
     def __init__(self):
         super().__init__()
-        self.buttons = [Button('Return', width // 2 - 60, height // 2 + 10 + 30 * 9, 20),
-                        Button('Choose level', width // 2 - 200, height // 2 - 20 * 10, 40, False)]
+        self.buttons = [Button('Return', width // 2 - 60+ 10, height // 2 + 10 + 30 * 9, 20),
+                        Button('Choose level', width // 2 - 230+ 10, height // 2 - 20 * 10, 40, False)]
         a = width // 2 - (1 + LEVELS // 2) * 86  # centre button
         self.con = sqlite3.connect("data/config/config.db")
         self.cur = self.con.cursor()
@@ -212,10 +204,8 @@ class Game(object):
     def update(self, pygame_event):
         """handle pygame events"""
         if pygame_event.type == pygame.KEYDOWN and pygame.key.get_pressed()[pygame.K_ESCAPE]:
-            global is_playing, on_pause
-            is_playing = False
-            on_pause = True
-            mouse.display = True
+            global current_window
+            current_window = 4
         if pygame_event.type == pygame.MOUSEBUTTONDOWN:
             self.unlock_level(2)
 
@@ -233,8 +223,8 @@ class Pause(Menu):
     def __init__(self):
         super().__init__()
         self.font = pygame.font.Font('data/fonts/8-BIT WONDER.TTF', 6)
-        self.buttons = [Button('Continue', width // 2 - 75, height // 2 - 30, 20),
-                        Button('Return to main menu', width // 2 - 180, height // 2 + 30, 20)]
+        self.buttons = [Button('Continue', width // 2 - 75+ 10, height // 2 - 30, 20),
+                        Button('Return to main menu', width // 2 - 180+ 10, height // 2 + 30, 20)]
 
     def render(self):
         """what to do on game cycle"""
@@ -251,31 +241,11 @@ class Pause(Menu):
     def update(self, pygame_event):
         """handle pygame events"""
         if pygame_event.type == pygame.KEYDOWN and pygame.key.get_pressed()[pygame.K_ESCAPE]:
-            global is_playing, on_pause
-            is_playing = True
-            on_pause = False
+            global current_window
+            current_window = 3
             mouse.display = False
         for i in self.buttons:
             i.update(pygame_event)
-
-
-def all_window_render():
-    """what to do on game cycle"""
-    data = [(display_menu, menu), (display_credits, titres),
-            (display_levels, level_menu), (is_playing, game), (on_pause, pause)]
-
-    for i, k in data:
-        if i:
-            k.render()
-
-
-def all_window_update(pygame_event):
-    """handle pygame events"""
-    data = [(display_menu, menu), (display_credits, titres),
-            (display_levels, level_menu), (is_playing, game), (on_pause, pause)]
-    for i, k in data:
-        if i:
-            k.update(pygame_event)
 
 
 if __name__ == '__main__':
@@ -284,7 +254,6 @@ if __name__ == '__main__':
     clock = pygame.time.Clock()
     running = True
     all_sprites = pygame.sprite.Group()
-
     menu = Menu()
     titres = Credits()
     level_menu = LevelChooser()
@@ -292,16 +261,19 @@ if __name__ == '__main__':
     pause = Pause()
     pygame.mouse.set_visible(False)
     mouse = Mouse(all_sprites)
+    data = {0: menu, 1: titres, 2: level_menu, 3: game, 4: pause}
     while running:
+
+        mouse.display = False if current_window == 3 else True
         screen.fill((0, 0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-            all_window_update(event)
+            data[current_window].update(event)
             all_sprites.update(event)
 
-        all_window_render()
+        data[current_window].render()
         all_sprites.draw(screen)
 
         pygame.display.flip()
